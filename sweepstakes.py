049176431fb85371cd09e80de1e732a82ec65b12
Mega Farts <clint.m.thompson@outlook.com>
2021-05-26 11:15:15 -0500

    Added a dictionary of contestants to the sweepstakes class

--- sweepstakes.py
+++ sweepstakes.py
@@ -6,6 +6,27 @@ class Sweepstakes:
     def __init__(self, name):
         self.name = name
 
+        self.contestants_list = [
+            {
+                "first_name": "Bill",
+                "last_name": "Dauterive",
+                "email": "bill4387@gmail.com",
+                "registration_number": 1
+            },
+            {
+                "first_name": "Hank",
+                "last_name": "Hill",
+                "email": "propane_god@strickland.com",
+                "registration_number": 2
+            },
+            {
+                "first_name": "Rusty",
+                "last_name": "Shackleford",
+                "email": "dales_dead_bug@hotmail.com",
+                "registration_number": 3
+            },
+        ]
+
     def register_contestant(self, contestant):
         pass
 
